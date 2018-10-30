808a5271fd0f8034e7ccba88b3159d6eab0019e1
Nicolas Holzman <nicolasholzman@hotmail.com>
2018-10-30 07:53:59 -0300

    Update README.js

--- README.js
+++ README.js
@@ -1,2 +1,3 @@
 //# order-names-numbers
-const orderWords = words => words.split(' ').sort(function(a, b){return a.match(/\d+/g) - b.match(/\d+/g)}).join(" ");
+const orderWords = words => words.split(' ').sort(function(a, b){return a.match(/\d+/g) - b.match(/\d+/g)}).join(" ").replace(/\d+/g, '');
+// 'ho2w h1i a3re tod5ay y4ou' => 'hi how are you'
